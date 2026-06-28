e86f49b310637d50b0520cce13acbe6323560a66
abhi <abhisiktachakraborty3@gmail.com>
2026-06-28 15:09:25 +0530

    feat: add annotation

--- fraud-service/src/main/java/com/finflow/fraud_service/exception/GlobalExceptionHandler.java
+++ fraud-service/src/main/java/com/finflow/fraud_service/exception/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.finflow.fraud_service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, Object>> handleRuntimeException(
--- notification-service/src/main/java/com/finflow/notification_service/exception/GlobalExceptionHandler.java
+++ notification-service/src/main/java/com/finflow/notification_service/exception/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.finflow.notification_service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, Object>> handleRuntimeException(
